99731362e05b09042dad531d1a7f3cbc34dc1b67
Kamet <kametguler@gmail.com>
2022-04-26 23:57:54 +0300

    fixed typos

--- profiles_project/profiles_api/models.py
+++ profiles_project/profiles_api/models.py
@@ -11,7 +11,7 @@ class UserProfileManager(BaseUserManager):
         if not email:
             raise ValueError("Users must have an email address.")
 
-        email = self.normalized_email(email)
+        email = self.normalize_email(email)
         user = self.model(email=email, name=name)
 
         user.set_password(password)
@@ -20,7 +20,7 @@ class UserProfileManager(BaseUserManager):
 
         return user
 
-    def create_super_user(self, email, name, password):
+    def create_superuser(self, email, name, password):
         """Creates and saves a new superuser with given details"""
 
         user = self.create_user(email, name, password)
