3966162dbc236981b63704838e06bbbbf3fc4442
Kamet <kametguler@gmail.com>
2022-04-27 15:41:50 +0300

    Added login API

--- profiles_project/profiles_api/urls.py
+++ profiles_project/profiles_api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import HelloApiView, HelloViewSet, UserProfileViewSet
+from .views import HelloApiView, HelloViewSet, UserProfileViewSet, LoginViewSet
 
 router = DefaultRouter()
 router.register('hello-viewset', HelloViewSet,
                 basename='hello-viewset')
 router.register('profile', UserProfileViewSet, basename='User-Profile')
+router.register('login', LoginViewSet, basename='login')
 
 urlpatterns = [
     path("hello/", HelloApiView.as_view(), name="hello"),
--- profiles_project/profiles_api/views.py
+++ profiles_project/profiles_api/views.py
@@ -5,6 +5,9 @@ from rest_framework import viewsets
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+
 
 from . import serializers
 from .models import UserProfile
@@ -112,3 +115,14 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
+
+
+class LoginViewSet(viewsets.ViewSet):
+    """Checks email and password and returns an auth token."""
+
+    serializer_class = AuthTokenSerializer
+
+    def create(self, request):
+        """Use the ObtainAuthToken APIView to validate and create a token"""
+
+        return ObtainAuthToken().as_view()(request=request._request)
